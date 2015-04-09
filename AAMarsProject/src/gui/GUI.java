c416a0366fd8c2c021f0a4c4fbb22dd4e8cd2f7d
degre028 <degre028@d.umn.edu>
2015-04-08 21:47:23 -0500

    Built bits and pieces of the GUI class.  Created Configuration.java

--- AAMarsProject/src/gui/GUI.java
+++ AAMarsProject/src/gui/GUI.java
@@ -1,5 +1,31 @@
 package gui;
 
-public class GUI {
+import backend.Configuration;
 
+/**
+ * Large, primary class for the GUI aspects of the lab.  
+ * Each GUI should contain and build all visual elements of the
+ * MARS simulator.
+ * @author Milan
+ *
+ */
+
+public class GUI {
+	/**
+	 * Global variable for the live Configuration.  
+	 * Modules and such are referred to by public methods in liveConfig.
+	 */
+	private Configuration liveConfig;
+	
+	
+	/**
+	 * Constructor for GUI, takes in the default configuration and assigns it, should instantiate the GUI classes
+	 * and build the UI displayed in the onModuleLoad method (main method).
+	 */
+	public GUI(Configuration passedConfig) {
+		liveConfig = passedConfig;
+		
+		//Code for GUI here.
+	}
+	
 }
