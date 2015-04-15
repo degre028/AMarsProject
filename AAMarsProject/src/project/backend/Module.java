186c9cd9ea060fbd5461870eba5e26834ad34efa
degre028 <degre028@d.umn.edu>
2015-04-15 13:44:28 -0500

    Team Meeting Preview Commit

--- AAMarsProject/src/project/backend/Module.java
+++ AAMarsProject/src/project/backend/Module.java
@@ -1,5 +1,23 @@
 package project.backend;
 
-public class Module {
 
+/**
+ * The module class represents a module the mars team may encounter.  Each time the user adds a module
+ * a new instance of the module class is created.
+ * @author Rob
+ *
+ */
+public class Module {
+	//Private fields for the module class.
+	int xcoord = -1;
+	
+	
+	
+	/**
+	 * Constructor for Module.
+	 */
+	public Module() {
+		
+	}
+	
 }
