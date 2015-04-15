ab5d861f07efe3d60a8a9950669aa8e757a6269a
jaros072 <jaros072@d.umn.edu>
2015-04-15 17:14:10 -0500

    yes

--- AAMarsProject/src/project/backend/Module.java
+++ AAMarsProject/src/project/backend/Module.java
@@ -10,13 +10,138 @@ package project.backend;
 public class Module {
 	//Private fields for the module class.
 	int xcoord = -1;
-	
+	int ycoord = -1;
+	int id = 0;
+	String condition = "";
+	int orientation = 0;
+	String type = "";
 	
 	
 	/**
-	 * Constructor for Module.
+	 * Constructor for the Module.
 	 */
-	public Module() {
+	public Module() {					
+	}
+	/**
+	 * Secondary Constructor for Module.
+	 */
+	public Module(int xdim, int ydim, int idnum, String cond, int ori) {
+		xcoord=xdim;
+		ycoord=ydim;
+		id=idnum;
+		condition=cond;
+		orientation=ori;				
+	}
+	
+	/**
+	 * Sets an integer value to the module
+	 * Sets the type of module based on the int ID Number.
+	 * @param x
+	 * @method makeType
+	 */
+	public void SetID(int x) {
+		id=x;
+		type=makeType();
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	/**
+	 * 
+	 * @return the id of the module
+	 */
+	public int getID() {
+		return id;
+	}
+	
+	/**
+	 * Sets the condition of the module in a string
+	 * @param cond
+	 */
+	public void setCondition(String cond) {
+		condition = cond;
+	}
+	
+	/**
+	 * 
+	 * @return The condition of the module as a String.
+	 */
+	public String getCondition() {
+		return condition;
+	}
+	
+	/**
+	 * Sets the orientation of the module as a 
+	 * @param ori
+	 */
+	public void setOrientation(int ori) {
+		orientation = ori;
+	}
+	
+	/**
+	 * 
+	 * @return the orientation of the module as an int. (number of flips needed to be upright)
+	 */
+	public int getOrientation() {
+		return orientation;
+	}
+	
+	/**
+	 * Sets the y-coordinate of the module as an int.
+	 * @param y
+	 */
+	public void setY(int y) {
+		ycoord = y;
+	}
+	
+	/**
+	 * 
+	 * @return the ycoordinate of the module as an int
+	 */	
+	public int getY() {
+		return ycoord;
+	}
+	
+	/**
+	 * 
+	 * @return the ID of the module as the type it actually is - String value.
+	 */
+	private String makeType() {
+		if(id > 0 && id < 41) {
+			return "Plain";
+		}
+		else if(id > 60 && id < 81) {
+			return "Dormitory";
+		}
+		else if(id > 90 && id < 101) {
+			return "Sanitation";
+		}
+		else if(id > 110 && id < 121) {
+			return "Food & Water";
+		}
+		else if(id > 130 && id < 135) {
+			return "Gym & Relaxation";
+		}
+		else if(id > 140 && id < 145) {
+			return "Canteen";
+		}
+		else if(id > 150 && id < 155) {
+			return "Power";
+		}
+		else if(id > 160 && id < 165) {
+			return "Control";
+		}
+		else if(id > 170 && id < 175) {
+			return "Airlock";
+		}
+		else if(id > 180 && id < 185) {
+			return "Medical";
+		}
+		else {
+			return "";
+		}
 		
 	}
 	
--- AAMarsProject/src/project/backend/ModuleSet.java
+++ AAMarsProject/src/project/backend/ModuleSet.java
@@ -1,5 +1,7 @@
 package project.backend;
 
+import java.util.LinkedList;
+
 
 /**
  * This class is the class that represents the current list of modules the user has entered.
@@ -11,13 +13,26 @@ package project.backend;
 public class ModuleSet {
 	//Private variables
 	
-	
+	LinkedList<Module> modList;
 	
 	/**
 	 * Constructor for ModuleSet.  This class will instantiate the first configuration.
 	 */
 	public ModuleSet() {
-		
+		modList = new LinkedList<Module>();
+	}
+	
+	public void addModule(Module mod) {
+		modList.add(mod);
+	}
+	
+	public int getCount(String type) {
+		int count = 0;
+		for(int i=0; i<modList.size(); i++)
+			if(modList.get(i).getType().equals(type)) {
+				count++;
+			}
+			return count;
 	}
 	
 }
